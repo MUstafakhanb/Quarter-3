bb13d8705070a57e0977615289f0b81f3b87d572
MUstafakhanb <mustafaqaiser759@gmail.com>
2025-05-05 18:54:31 +0500

    main.py

--- secure_data_encryption/main.py
+++ secure_data_encryption/main.py
@@ -85,7 +85,7 @@ elif choice == "Register":
         else:
             st.error("Both fields are required.")
 
-    elif choice == "Login":
+elif choice == "Login":
         st.subheader(" 🗝️ User Login")
 
         if time.time() < st.session_state.lockout_time:
