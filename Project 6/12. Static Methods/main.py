4d0a44ab3ed90fb8ccd5c926fbb0d3e77b1fe58f
MUstafakhanb <mustafaqaiser759@gmail.com>
2025-05-08 20:09:43 +0500

    main.py

--- Project 6/12. Static Methods/main.py
+++ Project 6/12. Static Methods/main.py
@@ -2,7 +2,8 @@ class TemperatureConverter:
     @staticmethod
     def celsius_to_fahrenheit(c):
         return (c * 9/5) + 32
-    
-    temp_c = 25
-    temp_f = TemperatureConverter.celsius_to_fahrenheit(temp_c)
-    print(f'{temp_c} C is equal to {temp_f} F')
+
+# Example usage
+temp_celsius = 12
+temp_fahrenheit = TemperatureConverter.celsius_to_fahrenheit(temp_celsius)
+print(f"{temp_celsius}°C is equal to {temp_fahrenheit}°F")
